b379eea82729a2351a2b50ac64ba24b39b4e82d0
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2026-03-05 22:59:38 -0700

    fix

--- routes/reviewProductRoutes.js
+++ routes/reviewProductRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const router = express.Router();
 const reviewProductControllers = require("../controllers/reviewProductController");
 const { protect } = require("../middlewares/auth");
 
-const router = express.Router();
+
+
 
 router.post("/", protect, reviewProductControllers.createReview);
-// router.get("/", reviewProductControllers.getReviews);
-// router.get("/:productId", reviewProductControllers.getReviewsByProductId);
 
 module.exports = router;
